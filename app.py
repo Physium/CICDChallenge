1da85779989fc526f4b4e8572df547041e1541dd
Wei Jun Loh <wjloh91@gmail.com>
2020-08-23 02:55:04 +0800

    testing build triggers with pull request

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_hit_count():
 
 @app.route('/feature')
 def feature():
-    return "Feature is up. Testing Multi-Pipeline Again."
+    return "Feature is up. Testing Build Triggers with Pull Request."
 
 
 @app.route('/test')
